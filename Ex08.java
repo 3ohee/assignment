62f05163ece8face4ede5c14f2b2b4d93993439e
최소희 <jsoy010707@gmail.com>
2024-08-22 09:41:35 +0900

    로또 수정

--- Ex08.java
+++ Ex08.java
@@ -5,20 +5,26 @@ import java.util.Set;
 
 public class Ex08 {
 	public static void main(String[] args) {
-		
-		for (int i = 1; i < 6; i++) {
+		// 로또 생성기 (5줄, 보너스번호까지)
+		for (int i = 0; i < 5; i++) {
 			Set<Integer> lottoNum = new HashSet<Integer>();
 			while(lottoNum.size() < 6) {
 				int number = (int)(Math.random()*45) + 1; // 1-45까지
 				lottoNum.add(number);
 				
 			}
-			int bonus = (int)(Math.random()*45) + 1;
-	
-			System.out.print("Lotto Set " + i + ": ");
+			// 보너스 번호 생성
+			int bonus;
+			
+			// 로또 넘버에 있는지 없는지 검사
+			do {
+				bonus = (int)(Math.random()*45) + 1;
+			} while (lottoNum.contains(bonus));
+			
+			System.out.print("Lotto Set " + (i+1) + ": ");
 			System.out.print(lottoNum);
 			System.out.println(" + Bonus: " + bonus);
 
-		}
+		} 
 	}
 }
